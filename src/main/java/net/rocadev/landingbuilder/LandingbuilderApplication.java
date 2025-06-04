6426e1b1fd30460e283ef94757db9ca7db2bcfa4
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2025-06-03 23:28:25 -0500

    Creating java application and pom.xml file

---
+++ src/main/java/net/rocadev/landingbuilder/LandingbuilderApplication.java
@@ -0,0 +1,11 @@
+package net.rocadev.landingbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandingbuilderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LandingbuilderApplication.class, args);
+    }
+}

5230654cbe15112b0320595697801f9a0dfb8e1b
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2025-06-17 23:14:15 -0500

    LandingBuilder: Add dependencies for JWT and Password Encryption

---
+++ src/main/java/net/rocadev/landingbuilder/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package net.rocadev.landingbuilder.repository;
+
+import net.rocadev.landingbuilder.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // Custom query method
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}

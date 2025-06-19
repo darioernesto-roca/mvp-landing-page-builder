a615f697490d5563851aef8cf0e476ac60040bda
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2025-06-18 22:59:49 -0500

    LandingBuilder: Added dto Login and Register Request

---
+++ dto/LoginRequest.java
@@ -0,0 +1,6 @@
+package net.rocadev.landingbuilder.dto;
+
+public class LoginRequest {
+    public String email;
+    public String password;
+}
---
+++ dto/RegisterRequest.java
@@ -0,0 +1,6 @@
+package net.rocadev.landingbuilder.dto;
+
+public class RegisterRequest {
+    public String email;
+    public String password;
+}
